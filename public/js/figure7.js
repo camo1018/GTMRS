72a862806bb1315b391e4ecbee84d1b8e229c1fd
jrapoport6 <jrapoport@bellsouth.net>
2014-04-19 13:08:30 -0400

    figure7.js
    errors show before clicking button…needs to be fixed

--- public/js/figure7.js
+++ public/js/figure7.js
@@ -6,82 +6,31 @@ $(function() {
 	// .validationError is a JQuery selector word that will find all HTML elements on the page whose CSS class is 'validationError'
 	$('.validationError').hide();
 
-	var allergies = new Array();
 
-	// This will bind a click eventhandler to the Submit Button.  Everytime the Submit button is clicked, this function will be executed.
-	$('#submitButton').bind('click', function() {
-		var name = $('#nameField').val();
-		var dateOfBirth = $('#dobField').val();
-		var gender = $('#genderOption').val();
-		var address = $('#addressField').val();
-		var homePhone = $('#homePhoneField').val();
-		var workPhone = $('#workPhoneField').val();
-		var weight = $('#weightField').val();
-		var height = $('#heightField').val();
-		var income = $('#incomeOption').val();
-
+	// This will bind a click eventhandler to the Checkout Button.  Everytime the Checkout button is clicked, this function will be executed.
+	$('#checkoutButton').bind('click', function() {
+		var medication = $('#medsField').val();
+		var doctor = $('#docField').val();
+	
 		// Validation
 		var validationError = false;
 
-		if (name == '') {
-			$('#nameValidationError').show();
+		if (medication == '') {
+			$('#medsValidationError').show();
 			validationError = true;
 		}
-		if (dateOfBirth == '') {
-			$('#dobValidationError').show();
+		if (doctor == '') {
+			$('#docValidationError').show();
 			validationError = true;
 		}
-		if (address == '') {
-			$('#addressValidationError').show();
-			validationError = true;
-		}
-		if (homePhone == '') {
-			$('#homePhoneValidationError').show();
-			validationError = true;
-		}
-		if (workPhone == '') {
-			$('#workPhoneValidationError').show();
-			validationError = true;
-		}
-		if (weight == '') {
-			$('#weightValidationError').show();
-			validationError = true;
-		}
-		if (height == '') {
-			$('#heightValidationError').show();
-			validationError = true;
+	
 		}
 
-		// Submit!
+		// Checkout!
 
 		// If validation failed, then don't actually submit anything.
 		if (validationError)
 			return;
 
-
-
 	});
-
-	// Everytime we click the Add button under Allergies, we will append html text into the document so that we will have a new row that describes an allergy.
-	$('#addAllergyButton').bind('click', function() {
-		var allergyName = $('#allergyField').val();
-
-		// If there is already an allergy of the same name, then don't add it.
-		if (allergies.indexOf(allergyName) != -1)
-			return;
-
-		$('#allergyList').append('<tr class="allergyListRow">' +
-            '<td id="allergyElement" class="allergyListElementOffset">' + allergyName + '</td>' +
-            '<td><button id="remove' + allergyName + '" type="button">-</button></td>' +
-          '</tr>');
-
-		$('#remove'+allergyName).bind('click', function() {
-			var allergyElementName = $(this).parent().find('#allergyElement').val();
-			allergies.splice(allergies.indexOf(allergyElementName),1);
-			$(this).parent().remove();
-		})
-
-		allergies.push($('allergyField').val())
-	});
-
 });
