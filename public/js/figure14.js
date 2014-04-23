9613edbdfc4353b5ee1a897816ff60d624f79294
jpodoloff <jpodoloff@comcast.net>
2014-04-23 16:54:46 -0400

    figure 14 links to 15

---
+++ public/js/figure14.js
@@ -0,0 +1,36 @@
+// Wait for DOM ready
+// Put all code inside this function.  This function is a JQuery function that will make sure the page is loaded fully first before executing any code.
+$(function() {
+
+	// Originally, the HTMLs are set to show, so let's hide that all.
+	// .validationError is a JQuery selector word that will find all HTML elements on the page whose CSS class is 'validationError'
+	$('.validationError').hide();
+
+
+	// This will bind a click eventhandler to the Submit Button.  Everytime the Submit button is clicked, this function will be executed.
+	$('#searchButton').bind('click', function() {
+		var name = $('#Name').val();
+		var phone = $('#phoneNum').val();
+
+
+		// Validation
+		var validationError = false;
+
+		$('.validationError').hide();
+		if ((name == '') && (phone == '')) {
+			$('#namephoneValidationError').show();
+			validationError = true;
+		}
+
+
+		// Submit!
+
+		// If validation failed, then don't actually submit anything.
+		if (validationError)
+			return;
+
+
+
+	});
+
+});
