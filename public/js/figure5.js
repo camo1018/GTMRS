a17767c8d44c4248a4d3f089d76b39be8e699293
Paul Park <camo1018@yahoo.com>
2014-04-24 04:21:16 -0400

    Some new libraries and fixed where figure5 would say hi

--- public/js/figure5.js
+++ public/js/figure5.js
@@ -2,7 +2,6 @@ $(function() {
 
 	var parameters = { username: serverData.username };
 	$.get('/patienthome/getMessageCount', parameters, function(data) {
-		alert('hi');
 		$('#messagesMessage').html('You have ' + data + ' unread messages.');
 	});
 
