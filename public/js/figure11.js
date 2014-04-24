d34f72f4d8693938b7e2e1b54aa489ca4452d657
titmouse903 <titmouse903@hotmail.com>
2014-04-24 15:49:01 -0400

    fig11 updated

--- public/js/figure11.js
+++ public/js/figure11.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	var parameters = { username: serverData.username };
-	$.get('/patienthome/getMessageCount', parameters, function(data) {
+	$.get('/doctorthome/getMessageCount', parameters, function(data) {
 		$('#messagesMessage').html('You have ' + data + ' unread messages.');
 	});
 
