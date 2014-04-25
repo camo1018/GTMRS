5c28540a39b53d3be1a0d15158987d337c56bea2
titmouse903 <titmouse903@hotmail.com>
2014-04-24 20:48:02 -0400

    server updated

--- server.js
+++ server.js
@@ -538,12 +538,26 @@ app.get('/rate/getDoctors', function(req, res) {
 })
 
 
+
+// Figure 5. Patient Homepage
+app.get('/patienthome/getMessageCount', function(req, res) {
+	console.log('Getting message count from the server.');
+	var username = req.query.username;
+	var query = 'SELECT COUNT(*) AS Count FROM Sends_Message_To_Patient WHERE ReceiverPUsername = \'' + username +'\'';
+	connection.query(query, function(err, rows, fields) {
+		if (err) throw err;
+		var count = rows[0].Count;
+		res.send(''+count);
+		console.log("Returning count " + count);
+	});
+});
+
 // Figure 11. Doctor Homepage
 app.get('/doctorhome/getMessageCount', function(req, res) {
 	console.log('Getting message count from the server.');
 	var username = req.query.username;
-	var query = 'SELECT COUNT(*) AS Count FROM Sends_Message_To_Patient OUTER JOIN Sends_Message_To_Doctor 
-    WHERE ReceiverPUsername = \'' + username +'\'' AND 'ReceiverDUsername = \'' + username +'\'';
+	var query = 'SELECT COUNT(*) AS Count FROM Sends_Message_To_Doctor OUTER JOIN Communicates' + 
+    'WHERE ReceiverDUsername = \'' + username +'\' AND ReceiverUsername = \'' + username +'\'';
 	connection.query(query, function(err, rows, fields) {
 		if (err) throw err;
 		var count = rows[0].Count;
