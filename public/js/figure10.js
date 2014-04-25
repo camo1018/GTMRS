73d2ae479e4b6f0ced926f6d902c2da7ac03ff49
jrapoport6 <jrapoport@bellsouth.net>
2014-04-25 00:08:19 -0400

    messaging 17a works, 17b still in progress

--- public/js/figure10.js
+++ public/js/figure10.js
@@ -5,6 +5,7 @@ $(function() {
 	// Originally, the HTMLs are set to show, so let's hide that all.
 	// .validationError is a JQuery selector word that will find all HTML elements on the page whose CSS class is 'validationError'
 	$('.validationError').hide();
+	
 	var params = { pUsername: pUsername};
 	$.get('/rate/getDoctors', params, function(data) {
 		for(var i =0; i<data.length; i++){
@@ -12,7 +13,7 @@ $(function() {
 			var lName = data[i].lName;
 			var username = data[i].username;
 			$('#doctorSelect').append('<option value=' + username + '>Dr. ' + fName + ' ' + lName + '</option>');
-		}// Get doctors then populate the doctorSelect
+		}
 	});
 
 	// This will bind a click eventhandler to the Submit Button.  Everytime the Submit button is clicked, this function will be executed.
@@ -33,7 +34,6 @@ $(function() {
 		if (validationError)
 			return;
 
-		// TODO:  Get Doctor's username
 		var dUsername = $('#doctorSelect').val();
 		var rating = $('#rating').val();
 
